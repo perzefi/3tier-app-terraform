0f243d606d5e3527c4b2afd5dcceb5b1d601c58f
perzefi <perzefi@gmail.com>
2024-07-03 10:50:27 +0100

    defined security group for database

--- db.tf
+++ db.tf
@@ -13,4 +13,6 @@ resource "aws_db_instance" "mysql" {
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
   db_subnet_group_name = aws_db_subnet_group.mysql.name
+  vpc_security_group_ids = [aws_security_group.database.id]
+  
 } 
